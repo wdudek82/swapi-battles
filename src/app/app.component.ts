3eb72ff6a8bc96686bcfeadbfededd42a036c7a5
Wojciech Dudek <wdudek82@gmail.com>
2024-02-18 01:21:22 +0100

    SWAB-1: Create draft version of game board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Resources } from './models/swapi.models';
+import { DataService } from './services/data.service';
+import { ToastrService } from 'ngx-toastr';
+
+enum ViewStatus {
+  INITIAL = 'initial',
+  LOADING = 'loading',
+  ERROR = 'error',
+  SUCCESS = 'success',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'swapi-battles';
+  protected readonly ViewStatus = ViewStatus;
+  viewStatus = ViewStatus.INITIAL;
+
+  loadedData: any[] = [];
+
+  constructor(
+    private dataService: DataService,
+    private toastr: ToastrService,
+  ) {}
+
+  loadInitialResources(): void {
+    this.viewStatus = ViewStatus.LOADING;
+
+    this.dataService.loadInitialResources().subscribe({
+      next: (res) => {
+        console.log(res);
+
+        res.results.forEach((result) => {
+          this.loadedData.unshift({
+            type: result.url.includes('people') ? 'person' : 'starship',
+            uid: result.uid,
+            name: result.name,
+          });
+        });
+      },
+      error: (err) => {
+        console.error(err);
+
+        this.viewStatus = ViewStatus.ERROR;
+        this.toastr.error('Something went wrong...', 'Preloading failed');
+      },
+      complete: () => {
+        this.viewStatus = ViewStatus.SUCCESS;
+        this.toastr.success(`Resources preloaded successfully`, `Success`);
+      },
+    });
+  }
+
+  getRandomPerson(): void {
+    this.dataService.getRandomPerson().subscribe((data) => {
+      console.log('AppComponent | person:', data);
+    });
+  }
+
+  getRandomStarship(): void {
+    this.dataService.getRandomStarship().subscribe((data) => {
+      console.log('AppComponent | starship:', data);
+    });
+  }
+
+  isInitial(): boolean {
+    return this.viewStatus === ViewStatus.INITIAL;
+  }
+
+  isLoading(): boolean {
+    return this.viewStatus === ViewStatus.LOADING;
+  }
+
+  hasErrors(): boolean {
+    return this.viewStatus === ViewStatus.ERROR;
+  }
+
+  isSuccessful(): boolean {
+    return this.viewStatus === ViewStatus.SUCCESS;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,45 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatButton } from '@angular/material/button';
+import { NgxLoadingButtonsModule } from 'ngx-loading-buttons';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 const materialUIModules = [
-  MatSlideToggleModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatDivider,
+  MatProgressBar,
+  MatButton,
+  MatMenu,
+  MatMenuItem,
+  MatMenuTrigger,
+  MatIcon,
 ];
 
+const thirdPartyModules = [NgxLoadingButtonsModule, ToastrModule.forRoot()];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ...materialUIModules,
-    AppRoutingModule
+    ...thirdPartyModules,
+    AppRoutingModule,
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
