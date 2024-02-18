89f116ac69f66fa9c961e3a0b20e7aa035989ae7
Wojciech Dudek <wdudek82@gmail.com>
2024-02-18 01:21:01 +0100

    SWAB-1: Create types representing API data

---
+++ src/app/models/swapi.models.ts
@@ -0,0 +1,37 @@
+export enum Resources {
+  people = 'people',
+  starships = 'starships',
+}
+
+export interface Person {
+  mass: string;
+}
+
+export interface Starship {
+  crew: string;
+}
+
+export interface SwApiRes {
+  message: string;
+}
+
+export interface SwApiItemsListRes extends SwApiRes {
+  total_records: number;
+  previous: string | null;
+  next: string | null;
+  results: SwApiResult[];
+}
+
+export interface SwApiItemRes extends SwApiRes {
+  result: {
+    properties: { [key: string]: string };
+  };
+}
+
+export interface SwApiResult {
+  uid: string;
+  name: string;
+  url: string;
+  properties?: { [key: string]: string };
+}
+
