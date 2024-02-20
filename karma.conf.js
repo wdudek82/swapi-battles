bb5120d59f16f830c5f334dd928ef8f55d64e680
Wojciech Dudek <wdudek82@gmail.com>
2024-02-20 22:02:56 +0100

    github-actions: include karma config

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     restartOnFileChange: true
   });
 };
