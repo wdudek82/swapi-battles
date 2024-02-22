99bea91fbba3752cf16ba3803a19f31caeedfb8a
Wojciech Dudek <wdudek82@gmail.com>
2024-02-22 23:48:48 +0100

    refactoring: add and fix unit tests after refactoring

--- src/app/components/game-board-component/game-board-component.component.spec.ts
+++ src/app/components/game-board-component/game-board-component.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameBoardComponentComponent } from './game-board-component.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ToastrModule } from 'ngx-toastr';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PlayerCardsContainerComponent } from '../player-cards/player-cards-container.component';
+import { NgxLoadingButtonsModule } from 'ngx-loading-buttons';
 
 describe('GameBoardComponentComponent', () => {
   let component: GameBoardComponentComponent;
@@ -8,10 +14,11 @@ describe('GameBoardComponentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GameBoardComponentComponent]
+      declarations: [GameBoardComponentComponent, PlayerCardsContainerComponent],
+      imports: [HttpClientTestingModule, MatSlideToggleModule, ToastrModule.forRoot(), MatToolbarModule, NgxLoadingButtonsModule],
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(GameBoardComponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
