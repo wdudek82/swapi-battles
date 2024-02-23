3ea3a759f79a1b8bbda340d844b2307e686e7012
Wojciech Dudek <wdudek82@gmail.com>
2024-02-24 00:16:03 +0100

    Add base URL to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 // TODO: Cypress causes conflics with Karma/Jasmine.
 export default defineConfig({
   e2e: {
+    baseUrl: "https://wdudek82.github.io/swapi-battles/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
